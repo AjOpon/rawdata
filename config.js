a9ee22b3367f45f4bb8eb7d406a6377a0ac13a27
Alfred Opon <alfredaejae@gmail.com>
2016-11-04 13:11:54 +0300

    Change port var name
    Change env port name to app_port

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports= {
 	'database' : process.env.DB_URL,
- 	'port' : process.env.DB_PORT,//port for server requests
+ 	'port' : process.env.APP_PORT,//port for server requests
  	'mongoptions' : {
 	  'user': process.env.DB_USER,
 	  'pass': process.env.DB_PSWD
